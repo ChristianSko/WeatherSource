dd06a6ffc6939e0e02267abb8aa84a41287c3d20
Christian Skorobogatow <christiansko@icloud.com>
2025-03-02 19:07:57 +0100

    Initial Commit

---
+++ WeatherSource/WeatherSourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSourceApp.swift
+//  WeatherSource
+//
+//  Created by Christian Skorobogatow on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherSourceTests/WeatherSourceTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSourceTests.swift
+//  WeatherSourceTests
+//
+//  Created by Christian Skorobogatow on 2/3/25.
+//
+
+import Testing
+@testable import WeatherSource
+
+struct WeatherSourceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
